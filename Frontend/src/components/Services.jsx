35277466e9a671c1ed847375e58dd6737b6edfea
Sujan <rsujan140.in@gmail.com>
2025-11-11 22:28:42 +0545

    Updated Service.jsx

--- Frontend/src/components/Services.jsx
+++ Frontend/src/components/Services.jsx
@@ -14,38 +14,26 @@ useEffect(() => {
   let autoScrollEnabled = true;
   let autoScrollSpeed = 1.2;
   let scrollDirection = 1;
-  let rafId;
-  let intervalId;
+  let timeoutId;
 
   const atEnd = () =>
     Math.ceil(carousel.scrollLeft) >= carousel.scrollWidth - carousel.clientWidth;
   const atStart = () => Math.floor(carousel.scrollLeft) <= 0;
 
-  const autoScroll = () => {
+  function smoothAutoScroll() {
     if (autoScrollEnabled && !isDragging) {
-      carousel.scrollLeft += autoScrollSpeed * scrollDirection;
+      carousel.scrollTo({
+        left: carousel.scrollLeft + autoScrollSpeed * scrollDirection,
+        behavior: "smooth",
+      });
       if (atEnd()) scrollDirection = -1;
       else if (atStart()) scrollDirection = 1;
     }
-    rafId = requestAnimationFrame(autoScroll);
-  };
+    timeoutId = setTimeout(smoothAutoScroll, 25); // ~40fps
+  }
 
-  const isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent);
-
-  // Start either RAF (for normal browsers) or setInterval (for iOS)
-  const startAutoScroll = () => {
-    if (isIOS) {
-      intervalId = setInterval(() => {
-        if (autoScrollEnabled && !isDragging) {
-          carousel.scrollLeft += autoScrollSpeed * scrollDirection;
-          if (atEnd()) scrollDirection = -1;
-          else if (atStart()) scrollDirection = 1;
-        }
-      }, 16); // roughly 60fps
-    } else {
-      rafId = requestAnimationFrame(autoScroll);
-    }
-  };
+  // Start after layout settles (important for iOS)
+  const startTimeout = setTimeout(smoothAutoScroll, 1000);
 
   const startDrag = (x) => {
     isDragging = true;
@@ -74,12 +62,8 @@ useEffect(() => {
   carousel.addEventListener("mousemove", (e) => dragMove(e.pageX));
   window.addEventListener("mouseup", endDrag);
   carousel.addEventListener("mouseleave", endDrag);
-  carousel.addEventListener("touchstart", (e) => startDrag(e.touches[0].pageX), {
-    passive: true,
-  });
-  carousel.addEventListener("touchmove", (e) => dragMove(e.touches[0].pageX), {
-    passive: true,
-  });
+  carousel.addEventListener("touchstart", (e) => startDrag(e.touches[0].pageX), { passive: true });
+  carousel.addEventListener("touchmove", (e) => dragMove(e.touches[0].pageX), { passive: true });
   carousel.addEventListener("touchend", endDrag);
 
   const handleWheel = (e) => {
@@ -92,14 +76,12 @@ useEffect(() => {
   };
   carousel.addEventListener("wheel", handleWheel);
 
-  const startTimeout = setTimeout(startAutoScroll, 800);
-
   return () => {
-    cancelAnimationFrame(rafId);
-    clearInterval(intervalId);
+    clearTimeout(timeoutId);
     clearTimeout(startTimeout);
   };
 }, []);
+
 
 
 
