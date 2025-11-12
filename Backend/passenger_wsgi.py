d6fa7c3b42f92750d01687986439b2f99fc0338c
Sujan <rsujan140.in@gmail.com>
2025-11-12 23:43:01 +0545

    Add passenger_wsgi for Bisup deployment

--- Backend/passenger_wsgi.py
+++ Backend/passenger_wsgi.py
@@ -7,7 +7,7 @@ if project_home not in sys.path:
     sys.path.insert(0, project_home)
 
 # Set environment variable for Django settings
-os.environ['DJANGO_SETTINGS_MODULE'] = 'CodeVoraBackend.settings'  # <-- corrected
+os.environ['DJANGO_SETTINGS_MODULE'] = 'Backend.settings'  # <- correct path
 
 # Import Django WSGI application
 from django.core.wsgi import get_wsgi_application
