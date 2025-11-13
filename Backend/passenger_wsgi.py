46e4a36b8c5f4f13cf640a7feaa884a964911333
Sujan <rsujan140.in@gmail.com>
2025-11-13 20:25:34 +0545

    Updated passenger_wsgi.py 2

--- Backend/passenger_wsgi.py
+++ Backend/passenger_wsgi.py
@@ -1,14 +1,11 @@
-import sys
 import os
+import sys
 
-# Add the project folder containing the Django settings
-project_home = '/home/sujancom/CodeVora_Backend/Backend'
-if project_home not in sys.path:
-    sys.path.insert(0, project_home)
+# Path to your Django project (where manage.py is)
+sys.path.insert(0, '/home/sujancom/CodeVora_Backend/Backend')
+sys.path.insert(0, '/home/sujancom/CodeVora_Backend')
 
-# Set the settings module for Django
-os.environ['DJANGO_SETTINGS_MODULE'] = 'Backend.settings'
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Backend.settings')
 
-# Import Django's WSGI handler
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
