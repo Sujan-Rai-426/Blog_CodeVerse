6e95ef5414d6e9f8aee2f320df2ff4d615d5118d
Sujan <rsujan140.in@gmail.com>
2025-11-26 16:33:09 +0545

    User View added

--- Frontend/src/admin/Admin_Home.jsx
+++ Frontend/src/admin/Admin_Home.jsx
@@ -8,6 +8,7 @@ import Admin_Update_Data from "./Admin_Update_Data";
 import Admin_Settings from "./Admin_Settings";
 import Admin_View_Data from "./Admin_View_Data";
 import { useNavigate, useOutletContext } from "react-router-dom";
+import Admin_View_User from "./Admin_View_User";
 
 export default function Admin_Home() {
   const navigate = useNavigate();
@@ -33,6 +34,8 @@ export default function Admin_Home() {
         return <Admin_View_Data adminData={adminData} />;
       case "update":
         return <Admin_Update_Data adminData={adminData} />;
+      case "view_users":
+        return <Admin_View_User adminData={adminData} />;
       case "settings":
         return <Admin_Settings />;
       default:
--- Frontend/src/admin/Admin_sidebar.jsx
+++ Frontend/src/admin/Admin_sidebar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "../assets/css/Admin_Home.css";
-import { FaPlus, FaList, FaEdit, FaHome, FaCog, FaTimes } from "react-icons/fa";
+import { FaPlus, FaList, FaEdit, FaHome, FaCog, FaTimes, FaUser } from "react-icons/fa";
 
 export default function Admin_Sidebar({ sidebarOpen, setSidebarOpen, setActivePage, activePage }) {
   const menuItems = [
@@ -8,6 +8,7 @@ export default function Admin_Sidebar({ sidebarOpen, setSidebarOpen, setActivePa
     { name: "Add Data", icon: <FaPlus />, key: "add" },
     { name: "View Data", icon: <FaList />, key: "view" },
     { name: "Update Data", icon: <FaEdit />, key: "update" },
+    { name: "View Users", icon: <FaUser />, key: "view_users" },
     { name: "Settings", icon: <FaCog />, key: "settings" },
   ];
 
