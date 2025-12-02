57341f81012439749a377cd226fc2317b9cbd7d1
Sujan <rsujan140.in@gmail.com>
2025-12-02 16:27:05 +0545

    Access token expire time reduce to 5 minute

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -133,7 +133,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(days=7),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=14),
     "AUTH_HEADER_TYPES": ("Bearer",),
 }
