62e9dbe2a75ad8db0be5e1cf822185e343096427
Sujan <rsujan140.in@gmail.com>
2025-12-07 03:15:50 +0545

    Updated for CSRF token read before CORS block

--- Frontend/src/clients/User_Login.jsx
+++ Frontend/src/clients/User_Login.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import apiClient from "../config/apiClient";
 import { fetchClientCsrfToken } from "../config/apiClient";
