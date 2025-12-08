442f5af80f55db47033c854dc128d14dec713596
Sujan <rsujan140.in@gmail.com>
2025-12-08 21:41:07 +0545

    EMAIL OTP for user

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 import { Previous_Page_Provider } from './context/Previous_Page_Context.jsx';
-import { HelmetProvider } from 'react-helmet-async';
+// import { HelmetProvider } from 'react-helmet-async';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
