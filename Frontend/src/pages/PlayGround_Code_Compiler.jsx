6222291c1b7b5155d2cafc3c77b9db22cf331a85
Sujan <rsujan140.in@gmail.com>
2025-12-21 03:44:12 +0545

    Updated preview code compiler

--- Frontend/src/pages/PlayGround_Code_Compiler.jsx
+++ Frontend/src/pages/PlayGround_Code_Compiler.jsx
@@ -91,7 +91,7 @@ const PlayGround_Code_Compiler = () => {
                             <button className={`pcc-tab ${activeTab === 'html' ? 'active' : ''}`} onClick={() => setActiveTab('html')} style={{color: 'rgb(196 108 72)'}}>
                                 <i className="devicon-html5-plain colored" ></i> index.html
                             </button>
-                            <button className={`pcc-tab ${activeTab === 'css' ? 'active' : ''}`} onClick={() => setActiveTab('css')} style={{color: 'rgb(81 81 164)'}}>
+                            <button className={`pcc-tab ${activeTab === 'css' ? 'active' : ''}`} onClick={() => setActiveTab('css')} style={{color: 'rgb(90 130 224)'}}>
                                 <i className="devicon-css3-plain colored" ></i> style.css
                             </button>
                             <button className={`pcc-tab ${activeTab === 'js' ? 'active' : ''}`} onClick={() => setActiveTab('js')} style={{color: '#949454'}}>
